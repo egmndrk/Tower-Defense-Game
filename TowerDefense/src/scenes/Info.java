493364d759d1233d2ba4135fd2b6b0fc7bb07128
Egemen Doruk Serdar <egemendorukserdar@gmail.com>
2024-02-29 11:45:17 +0300

    Tower Defense Game
    Functional tower defense game written in Java and visualized with Swing

---
+++ TowerDefense/src/scenes/Info.java
@@ -0,0 +1,90 @@
+package scenes;
+
+import static main.GameStates.MENU;
+import static main.GameStates.SetGameState;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import main.Game;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+import ui.MyButton;
+import static main.GameStates.*;
+
+public class Info extends GameScene implements SceneMethods {
+	private MyButton bMenu;
+	private BufferedImage img;
+
+	
+
+	public Info(Game game) {
+		
+		super(game);
+		initButtons();
+		
+
+	}
+
+	
+		
+	
+
+	private void initButtons() {
+		bMenu = new MyButton("Menu", 2, 2, 100, 30);
+	}
+
+	@Override
+	public void render(Graphics g) {
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, 640, 640);
+
+		drawButtons(g);
+	}
+
+	private void drawButtons(Graphics g) {
+		bMenu.draw(g);
+	}
+
+	@Override
+	public void mouseClicked(int x, int y) {
+		if (bMenu.getBounds().contains(x, y))
+			SetGameState(MENU);
+
+	}
+
+	@Override
+	public void mouseMoved(int x, int y) {
+		bMenu.setMouseOver(false);
+		if (bMenu.getBounds().contains(x, y))
+			bMenu.setMouseOver(true);
+
+	}
+
+	@Override
+	public void mousePressed(int x, int y) {
+		if (bMenu.getBounds().contains(x, y))
+			bMenu.setMousePressed(true);
+	}
+
+	@Override
+	public void mouseReleased(int x, int y) {
+		resetButtons();
+	}
+
+	private void resetButtons() {
+		bMenu.resetBooleans();
+
+	}
+
+	@Override
+	public void mouseDragged(int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
